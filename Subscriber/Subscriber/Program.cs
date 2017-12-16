42a725d94a863901f9500789c8e6ffad936d81a8
Владислав Бирюков <biryukov.vlad2011@yandex.ru>
2017-12-16 15:33:50 +0500

    [~] New solution

--- Subscriber/Subscriber/Program.cs
+++ Subscriber/Subscriber/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace pubsubchat
+namespace Subscriber
 {
-    class Subscriber
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
